d7d448eb0969f6d5c8838fdbaeb9a00b8588567b
Titus Whang <tituswhang0930@gmail.com>
2023-10-22 22:27:53 -0400

    Added comments for explanation

--- Assets/Darwin/TriggerArea.cs
+++ Assets/Darwin/TriggerArea.cs
@@ -1,7 +1,7 @@
 ﻿/**
-This code is applied to the trigger collider attached to the frog itself.
-It is used when an object(either a food or another frog)
-enters the area near the frog. It is also responsible for keeping
+This code is applied to the trigger collider which is attached as a child to a Darwin.
+It is used when an object(either a food or another Darwin)
+enters the area near the current Darwin. It is also responsible for keeping
 track of all of the objects that are within the range.
 */
 
@@ -16,15 +16,21 @@ public class TriggerArea : MonoBehaviour
     public List<GameObject> cookiesInArea = new List<GameObject>();
     public List<GameObject> darwinsInArea = new List<GameObject>();
 
-    void OnTriggerEnter2D(Collider2D other) // Add objects to list when they get in range
+    /*
+     * This function is responsible for adding objects, either a cookie or another Darwin to the
+     * Lists cookiesInArea or darwinsInArea.
+     * 
+     * @param other: The Collider2D of the object that just entered the trigger area of the Darwin.
+     */
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Cookie")) // Cookie in range
+        if (other.gameObject.CompareTag("Cookie")) // The gameObject that just entered was a cookie
         {
             cookiesInArea.Add(other.gameObject);
             //umCookies++;
             //Debug.Log("TriggerArea - Cookie " + numCookies);
         }
-        else if (other.gameObject.CompareTag("Darwin")) // Frog in range
+        else if (other.gameObject.CompareTag("Darwin")) // The Collider2D that just entered was a Darwin
         {
             darwinsInArea.Add(other.gameObject);
             //numDarwins++;
@@ -32,15 +38,21 @@ public class TriggerArea : MonoBehaviour
         }
     }
 
-    void OnTriggerExit2D(Collider2D other) // Remove objects from list when they get out of range
+    /*
+     * This function is responsible for removing objects, either a cookie or another Darwin,
+     * from the Lists if they have exited the triggerArea.
+     * 
+     * @param other: The Collider2D of the object that just exited the trigger area of the Darwin.
+     */
+    void OnTriggerExit2D(Collider2D other)
     {
-        if(cookiesInArea.IndexOf(other.gameObject) >= 0) // Cookie left range
+        if(cookiesInArea.IndexOf(other.gameObject) >= 0) // The gameObject that just exited was a cookie
         {
             cookiesInArea.Remove(other.gameObject);
             //numCookies--;
             //Debug.Log(numCookies+"Cookie(s)");
         }
-        else if (darwinsInArea.IndexOf(other.gameObject) >= 0) // Frog left range
+        else if (darwinsInArea.IndexOf(other.gameObject) >= 0) // The gameObject that just exited was a Darwin
         {
             darwinsInArea.Remove(other.gameObject);
             //numDarwins--;
