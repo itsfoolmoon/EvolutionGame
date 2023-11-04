304e1023e54f6c26b9d7825ca953fd8b897c6f2e
Michael Singer <mis825@lehigh.edu>
2023-11-04 00:07:34 -0400

    Main Menu MVP

--- Assets/Instantiator/DarwinInstantiator.cs
+++ Assets/Instantiator/DarwinInstantiator.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class DarwinInstantiator : MonoBehaviour
 {
     public GameObject darwin;
+    private int darwinPopulation = 5;
 
-    void Start()
+    private void Start()
     {
-        for (int i = 0; i < 5; i++)
+        LoadSettings();
+        InstantiateDarwins();
+    }
+
+    private void LoadSettings()
+    {
+        if (SettingsManager.Instance != null)
+        {
+            darwinPopulation = SettingsManager.Instance.darwinPopulation;
+        }
+        else
+        {
+            Debug.LogWarning("SettingsManager.Instance is null, using default values");
+        }
+    }
+
+    private void InstantiateDarwins()
+    {
+        for (int i = 0; i < darwinPopulation; i++)
         {
             GameObject newObject = Instantiate(darwin, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0), Quaternion.identity);
             DarwinTraits traits = newObject.GetComponent<DarwinTraits>();
@@ -16,11 +35,11 @@ public class DarwinInstantiator : MonoBehaviour
             traits.Mass = Random.Range(0.05f, 0.5f);
             traits.ChargeCoolDown = Random.Range(1.0f, 5.0f);
             traits.ChargeStrength = Random.Range(100.0f, 200.0f);
-            traits.BoredThreshold = (byte) Random.Range(3, 6);
+            traits.BoredThreshold = (byte)Random.Range(3, 6);
             traits.DeltaDeviationAngle = Random.Range(0.0f, 15.0f);
-            traits.EnergyPerCharge = (byte) Random.Range(1.0f, 6.0f);
-            traits.BreedEnergy = (byte) Random.Range(40.0f, 101.0f);
-            traits.Energy = (short) Random.Range(15.0f, 51.0f);
+            traits.EnergyPerCharge = (byte)Random.Range(1.0f, 6.0f);
+            traits.BreedEnergy = (byte)Random.Range(40.0f, 101.0f);
+            traits.Energy = (short)Random.Range(15.0f, 51.0f);
 
             traits.MassHeredity = Random.Range(0.0f, 0.5f);
             traits.ChargeCoolDownHeredity = Random.Range(0.0f, 0.5f);
