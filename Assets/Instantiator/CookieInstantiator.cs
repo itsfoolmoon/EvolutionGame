59a29fe3977b34f84c31c23996bd88f13e581b2d
Titus Whang <tituswhang0930@gmail.com>
2023-11-06 01:20:56 -0500

    Implemented new Cookie spawning algorithm + cookies no longer spawn in walls

--- Assets/Instantiator/CookieInstantiator.cs
+++ Assets/Instantiator/CookieInstantiator.cs
@@ -6,14 +6,67 @@ public class CookieInstantiator : MonoBehaviour
 {
     public GameObject cookie;
 
+    // How many cookies are being eaten per second.
+    // Formula: cookiesEatenRate = cookiesEaten / (Time.time * cookieSpawnRate);
+    public float cookiesEatenRate = 0.0f;
+
+    // The frequency at which cookies spawn. Higher = faster cookie spawns
+    public float cookieSpawnRate = 10.0f;
+
+    // How long it takes for the next cookie to spawn
+    // Formula: nextCookieSpawnTime = Time.time + cookiesEatenRate;
+    public float nextCookieSpawnTime = 0.0f;
+
+    // The maximum amount of cookies to spawn
+    public int maxCookies = 75;
+
+    // The number of cookies eaten throughout the simulation.
+    public long CookiesEaten { get; set; }
+
     void Start()
     {
-        for (int i = 0; i < 75; i++)
-            spawnCookie();
+        CookiesEaten = 0;
+
+        // Spawn maxCookies amount of cookies
+        for (int i = 0; i < maxCookies; i++)
+        {
+            StartCoroutine(SpawnCookie());
+            CookiesEaten = 0;
+            cookiesEatenRate = 0.0f;
+            nextCookieSpawnTime = 0.0f;
+        }
     }
 
-    public void spawnCookie()
+    public IEnumerator SpawnCookie()
     {
-        Instantiate(cookie, new Vector3(Random.Range(-18.0f,18.0f), Random.Range(-18.0f, 18.0f), 0), Quaternion.identity);
+        Debug.Log("SpawnCookie started");
+        GameObject[] darwins = GameObject.FindGameObjectsWithTag("Darwin");
+        yield return new WaitForSeconds(darwins.Length);
+        Debug.Log("Waited for " + nextCookieSpawnTime + " seconds");
+
+        Instantiate(cookie, FindEmptySpace(), Quaternion.identity);
+        Debug.Log("Cookie spawned");
+
+        //cookiesEatenRate = CookiesEaten / (Time.time * cookieSpawnRate);
+        //nextCookieSpawnTime = Time.time + cookiesEatenRate;
+        //Debug.Log("Updated cookiesEatenRate and nextCookieSpawnTime");
+    }
+
+    private Vector3 FindEmptySpace()
+    {
+        Collider2D collided;
+
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-18.0f, 18.0f), Random.Range(-18.0f, 18.0f), 0);
+
+            // Check for collisions at the spawn position
+            collided = Physics2D.OverlapCircle(spawnPosition, 0.2f);
+
+            if (collided == null)
+            {
+                return spawnPosition;
+            }
+        }
     }
 }
