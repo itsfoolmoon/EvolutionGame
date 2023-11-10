a8e6608d257c7426faa51812339daa57c7dadf0c
Titus Whang <tituswhang0930@gmail.com>
2023-11-10 17:50:45 -0500

    Darwins now spawn with more dynamic traits

--- Assets/Instantiator/DarwinInstantiator.cs
+++ Assets/Instantiator/DarwinInstantiator.cs
@@ -32,23 +32,23 @@ public class DarwinInstantiator : MonoBehaviour
             GameObject newObject = Instantiate(darwin, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0), Quaternion.identity);
             DarwinTraits traits = newObject.GetComponent<DarwinTraits>();
 
-            traits.Mass = Random.Range(0.05f, 0.5f);
-            traits.ChargeCoolDown = Random.Range(1.0f, 5.0f);
+            traits.Mass = Random.Range(0.05f, 1f);
+            traits.ChargeCoolDown = Random.Range(0.5f, 10.0f);
             traits.ChargeStrength = Random.Range(100.0f, 200.0f);
             traits.Boredom = 0;
             traits.BoredThreshold = (byte) Random.Range(3, 6);
-            traits.DeltaDeviationAngle = Random.Range(0.0f, 15.0f);
+            traits.DeltaDeviationAngle = Random.Range(0.0f, 45.0f);
             traits.EnergyPerCharge = (byte)Random.Range(1.0f, 6.0f);
             traits.BreedEnergy = (byte)Random.Range(40.0f, 101.0f);
-            traits.Energy = (short)Random.Range(15.0f, 51.0f);
+            traits.Energy = (short) (traits.BreedEnergy / 2);
 
-            traits.MassHeredity = Random.Range(0.0f, 0.5f);
-            traits.ChargeCoolDownHeredity = Random.Range(0.0f, 0.5f);
-            traits.ChargeStrengthHeredity = Random.Range(0.0f, 0.5f);
-            traits.BoredThresholdHeredity = Random.Range(0.0f, 0.5f);
-            traits.EnergyPerChargeHeredity = Random.Range(0.0f, 0.5f);
-            traits.DeltaDeviationAngleHeredity = Random.Range(0.0f, 0.5f);
-            traits.BreedEnergyHeredity = Random.Range(0.0f, 0.5f);
+            traits.MassHeredity = Random.Range(0.325f, 0.5f);
+            traits.ChargeCoolDownHeredity = Random.Range(0.325f, 0.5f);
+            traits.ChargeStrengthHeredity = Random.Range(0.325f, 0.5f);
+            traits.BoredThresholdHeredity = Random.Range(0.325f, 0.5f);
+            traits.EnergyPerChargeHeredity = Random.Range(0.325f, 0.5f);
+            traits.DeltaDeviationAngleHeredity = Random.Range(0.325f, 0.5f);
+            traits.BreedEnergyHeredity = Random.Range(0.325f, 0.5f);
         }
     }
 }
